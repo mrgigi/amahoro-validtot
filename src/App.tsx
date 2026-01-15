1912531868518c3d5c95e84ba280aea20ddb4bf8
Alexander <18466531+mrgigi@users.noreply.github.com>
2026-01-15 18:59:36 +0200

    Add routes for CreatePost and AdminDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
+import CreatePost from "../Pages/CreatePost";
+import AdminDashboard from "../Pages/AdminDashboard";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
